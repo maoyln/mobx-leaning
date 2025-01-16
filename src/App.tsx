2b1b6e416e09672d9ca3f29f943fe23fabdeb1c2
maoyln <maoyl_yx@163.com>
2025-01-16 14:21:01 +0800

    mobx学习初

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
 import TodoListView from './pages/MobxState/TodoListView';
+import MobxDemo1 from './pages/01MobxDemo/MobxDemo1';
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/todoListView" element={<TodoListView />} />
+        <Route path="/mobxDemo1" element={<MobxDemo1 />} />
         <Route path="*" element={<NotFoundPage />} /> {/* 404 路由 */}
       </Routes>
     </Router>
---
+++ src/pages/01MobxDemo/MobxDemo1.tsx
@@ -0,0 +1,11 @@
+// import React from "react";
+
+const MobxDemo1 = () => {
+  return (
+    <div>
+      <h1>MobxDemo1</h1>
+    </div>
+  );
+};
+
+export default MobxDemo1;
