61270f8fc65f6035941f456393103a41f3dd8e7d
maoyln <maoyl_yx@163.com>
2025-01-16 17:06:49 +0800

    mobx-computed属性

--- src/pages/01MobxDemo/MobxDemo1.tsx
+++ src/pages/01MobxDemo/MobxDemo1.tsx
@@ -7,9 +7,15 @@ class User {
   name: string = 'myl';
   age: number = 18;
 
+
   constructor() {
     // 可观察的对象
-    makeAutoObservable(this);
+    makeAutoObservable(this, {}, { autoBind: true });
+  }
+
+  // 计算属性-计算出生年份-get当作属性使用 computed
+  get bornYear() {
+    return new Date().getFullYear() - this.age
   }
 
   // 动作方法
@@ -37,12 +43,15 @@ interface MyComponentProps {
 
 // 创建一个有观察能力的组件
 const MyComponent: React.FC<MyComponentProps> = observer(({ store }) => {
+  // 此处不能解构 ,如果非要解构，需要修改上述代码makeAutoObservable(this, {}, { autoBind: true });
+  const { name, age, bornYear, increaseAge, decrementAge } = store;
   return (
     <>
-      <h2>姓名: {store.name}</h2>
-      <h2>年龄: {store.age}</h2>
-      <button onClick={() => store.increaseAge()}>增加年龄</button>
-      <button onClick={() => store.decrementAge()}>减少年龄</button>
+      <h2>姓名: {name}</h2>
+      <h2>年龄: {age}</h2>
+      <h2>出生年份: {bornYear}</h2>
+      <button onClick={() => increaseAge()}>增加年龄</button>
+      <button onClick={() => decrementAge()}>减少年龄</button>
     </>
   );
 });
