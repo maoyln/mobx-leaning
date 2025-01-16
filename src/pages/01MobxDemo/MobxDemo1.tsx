75dfe271ca81a2464d1b7e331312240ba58f9593
maoyln <maoyl_yx@163.com>
2025-01-16 15:00:14 +0800

    mobx观察

--- src/pages/01MobxDemo/MobxDemo1.tsx
+++ src/pages/01MobxDemo/MobxDemo1.tsx
@@ -1,13 +1,15 @@
 import React from "react";
+import { makeAutoObservable } from 'mobx'
+import { observer } from "mobx-react";
 
 class User {
   // 状态（成员属性）
-  private name: string = 'myl';
-  private age: number = 18;
+  name: string = 'myl';
+  age: number = 18;
 
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
+  constructor() {
+    // 可观察的对象
+    makeAutoObservable(this);
   }
 
   // 动作方法
@@ -26,7 +28,24 @@ class User {
   }
 }
 
-const user = new User('myl', 18);
+// 得到一个可以被观察的示例对象
+const user: User = new User();
+
+interface MyComponentProps {
+  store: User;
+}
+
+// 创建一个有观察能力的组件
+const MyComponent: React.FC<MyComponentProps> = observer(({ store }) => {
+  return (
+    <>
+      <h2>姓名: {store.name}</h2>
+      <h2>年龄: {store.age}</h2>
+      <button onClick={() => store.increaseAge()}>增加年龄</button>
+      <button onClick={() => store.decrementAge()}>减少年龄</button>
+    </>
+  );
+});
 
 console.log(user);
 
@@ -34,6 +53,7 @@ const MobxDemo1: React.FC = () => {
   return (
     <div>
       <h1>MobxDemo1</h1>
+      <MyComponent store={user} />
     </div>
   );
 };
