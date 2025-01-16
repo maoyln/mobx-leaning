27cb4b1073005a4de1c3517b65f07b56c7a034e2
maoyln <maoyl_yx@163.com>
2025-01-16 14:28:19 +0800

    mobx学初始化项目

--- src/pages/01MobxDemo/MobxDemo1.tsx
+++ src/pages/01MobxDemo/MobxDemo1.tsx
@@ -1,6 +1,36 @@
-// import React from "react";
+import React from "react";
 
-const MobxDemo1 = () => {
+class User {
+  // 状态（成员属性）
+  private name: string = 'myl';
+  private age: number = 18;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  // 动作方法
+  changeName(name: string) {
+    this.name = name;
+  }
+
+  // 动作方法
+  increaseAge() {
+    this.age++;
+  }
+
+  // 动作方法
+  decrementAge() {
+    this.age--;
+  }
+}
+
+const user = new User('myl', 18);
+
+console.log(user);
+
+const MobxDemo1: React.FC = () => {
   return (
     <div>
       <h1>MobxDemo1</h1>
